aeb1fbe76fad3788d942639435209695e508b490
Victor Ezea <victorvikson@gmail.com>
2022-05-30 14:44:40 +0100

    Navigation Setup

---
+++ src/Navigations/Navigation.js
@@ -0,0 +1,23 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {OrderTrack, ConfirmationScreen, HomeScreen} from '../screens';
+
+const Stack = createStackNavigator();
+
+const ScreenOptions = {
+  headerShown: false,
+};
+
+export default function Navigation() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={ScreenOptions}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="OrderTrack" component={OrderTrack} />
+        <Stack.Screen name="Confirmation" component={ConfirmationScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
